bec94afa26d3e3df388cb9fcc368a4d019c744ba
AusA <coremanwow@gmail.com>
2024-02-03 01:41:17 -0300

    subiendo archivos

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+ACTIVIDAD 01
+
+Clonar el repositorio del proyecto y explorar su estructura. Identificar los dos archivos package.json de ambas partes del proyecto.
+
+ACTIVIDAD 02
+
+Instalar las dependencias de la aplicación de Front-End y probar el funcionamiento con el comando “npm start”. Esto debería “levantar” nuestra aplicación de front en el navegador.
+
+ACTIVIDAD 03
+
+Diseñar la página web con la que estaremos trabajando. Se trata de una página de películas, por lo que el diseño debe ser acorde a la temática. Esto implicará:
+
+    Generar los archivos css necesarios y vincularlos al documento index.html.
+
+    Generar la plantilla HTML con sus elementos y asignar las clases y ids correspondientes para seleccionarlos y darles estilos.
+
+ACTIVDAD 04
+
+Mapear los objetos incluídos en el array tempData para construir con sus datos las tarjetas correspondientes a cada película. Incluir estas tarjetas en el contenedor que hayas creado.
+
+ACTIVIDAD 05
+
+Incluir en la página web enlaces a páginas estáticas. Estas páginas estáticas (mínimo 2) tendrán una temática a tu elección. Sugerencias: “Sobre mi proyecto”, “Historia del cine”, “Webs recomendadas”.
+
+ACTIVIDAD 06
+
+Configurar correctamente los enlaces desde index.html para poder navegar hacia estas páginas, y luego desde las mismas poder regresar al index. 
